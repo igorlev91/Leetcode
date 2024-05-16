cff7017a739d08a6511f0b632696f93f70d88f24
Igor Levdansky <igorlevdansky91@gmail.com>
2024-05-16 12:09:30 +0300

    use two pointer approach for task "1. Two Sum"

--- Algorithms/0001.TwoSum/solution.h
+++ Algorithms/0001.TwoSum/solution.h
@@ -1,16 +1,6 @@
-#include <algorithm>
-#include <vector>
-
-#include "gtest/gtest.h"
-
-namespace
-{
-
-class Solution
-{
+class Solution {
 public:
-    std::vector<int> twoSum(std::vector<int> const &nums, int target) const
-    {
+    std::vector<int> twoSum(std::vector<int> const &nums, int target) {
         // create and sort vector of indices
         std::vector<int> indices;
         indices.reserve(nums.size());
@@ -24,33 +14,20 @@ public:
         int right = indices.size() - 1;
         while (left != right)
         {
-            const int value = nums[indices[left]] + nums[indices[right]];
+            int value = nums[indices[left]] + nums[indices[right]];
             if (value == target)
+            {
                 return {indices[left], indices[right]};
-            if (value < target)
+            }
+            else if (value < target)
+            {
                 ++left;
+            }
             else if (value > target)
+            {
                 --right;
+            }
         }
         return {};
     }
 };
-
-}
-
-namespace TwoSumTask
-{
-
-TEST(TwoSumTaskTests, Examples)
-{
-    const Solution solution;
-    ASSERT_EQ(std::vector<int>({0, 1}), solution.twoSum({2, 7, 11, 15}, 9));
-}
-
-TEST(TwoSumTaskTests, FromWrongAnswers)
-{
-    const Solution solution;
-    ASSERT_EQ(std::vector<int>({1, 2}), solution.twoSum({3, 2, 4}, 6));
-}
-
-}
