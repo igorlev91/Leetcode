c8a1fb253a659151063b414993af759d8be75975
IgorLev91 <igorlevdansky91@gmail.com>
2025-04-11 14:40:26 +0300

    added task "112. Path Sum" + made some improvements

--- Algorithms/CommonDefs/TreeNode.h
+++ Algorithms/CommonDefs/TreeNode.h
@@ -2,6 +2,8 @@
 
 #include <memory>
 
+#include "gtest/gtest.h"
+
 namespace CommonDefs
 {
 
@@ -22,6 +24,18 @@ struct TreeNode
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right)
     {
     }
+
+    TreeNode(int x, int leftValue, TreeNode *right) : val(x), left(new TreeNode(leftValue)), right(right)
+    {
+    }
+
+    TreeNode(int x, TreeNode *left, int rightValue) : val(x), left(left), right(new TreeNode(rightValue))
+    {
+    }
+
+    TreeNode(int x, int leftValue, int rightValue) : val(x), left(new TreeNode(leftValue)), right(new TreeNode(rightValue))
+    {
+    }
 };
 
 
@@ -37,6 +51,24 @@ void deleteTree(TreeNode *root)
 std::shared_ptr<TreeNode> createTreeHolder(TreeNode *root)
 {
     return std::shared_ptr<TreeNode>(root, deleteTree);
+}
+
+void checkTree(TreeNode *expected, TreeNode *actual)
+{
+    if (expected == nullptr)
+    {
+        ASSERT_TRUE(actual == nullptr);
+        return;
+    }
+    ASSERT_EQ(expected->val, actual->val);
+    checkTree(expected->left, actual->left);
+    checkTree(expected->right, actual->right);
+}
+
+void checkAndDeleteTree(TreeNode *expected, TreeNode *actual)
+{
+    checkTree(expected, actual);
+    deleteTree(actual);
 }
 
 }
